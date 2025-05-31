ca5752e1429e2f34c8b3f1ff32ce548e440a3c48
Manasseh2352 <manassehunachi@gmail.com>
2025-05-31 10:04:41 +0100

    feat: implement responsive menu toggle in NavBar component

--- app/components/NavBar/NavBar.tsx
+++ app/components/NavBar/NavBar.tsx
@@ -1,20 +1,34 @@
-
+'use client';
+import React from 'react';
 
 export default function Page() {
+    const [isOpen, setIsOpen] = React.useState(false);
+    const toggleMenu = () => {
+        setIsOpen(!isOpen);
+    };
+
     return (
         <div className="flex justify-center mt-10">
-            <nav className=" flex justify-between gap-96">
+            <nav className=" flex justify-between gap-70">
                 <div>
                     <h1>TIPJAR</h1>
                 </div>
 
-                <div>
+                <div className='hidden md:flex'>
                     <ul className="flex gap-5">
                         <li>Home</li>
                         <li>How it works</li>
                         <li>Leaderboard</li>
                         <li className="bg-blue-600 p-1 rounded">Conect Wallet</li>
                     </ul>
+                </div>
+                <div>
+                    <button
+                        className="md:hidden p-2 rounded bg-blue-600"
+                        onClick={toggleMenu}
+                    >
+                        {isOpen ? 'Close' : 'Menu'}
+                    </button>
                 </div>
             </nav>
         </div>
